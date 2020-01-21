8539dff00d2f3353c6052cca1e5e6c0ea6a6fb71
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-01-21 23:03:18 +0100

    Updates

---
+++ precache-manifest.a8d300011ccce934f86b3549e2d3e22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b20432e527421c1ad234830c77515c8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d777f2c38bc39a807670",
+    "url": "/calculator/static/css/main.7d0776c8.chunk.css"
+  },
+  {
+    "revision": "02484cad65be0ec43c7f",
+    "url": "/calculator/static/js/2.896b662b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.896b662b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d777f2c38bc39a807670",
+    "url": "/calculator/static/js/main.3963a701.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
